04bc119f070f2b843eaa1b7b19dec696c18d863d
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-12 14:10:11 -0500

    feat: set configuration for endpoint 'createUser'

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
+import { CreateUserDto } from '../dtos/users.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +10,14 @@ export class UsersController {
   async findAll() {
     const res = await this.usersService.findAll();
     return res;
+  }
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    const res = await this.usersService.create(createUserDto);
+    return {
+      message: 'User created successfully',
+      data: res,
+    };
   }
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -2,6 +2,7 @@ import { User } from '@entities/users/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dtos/users.dto';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +13,11 @@ export class UsersService {
   async findAll() {
     const usersList = await this.userRepo.find();
     return usersList;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const newUser = this.userRepo.create(createUserDto);
+    const createdUser = await this.userRepo.save(newUser);
+    return createdUser;
   }
 }
