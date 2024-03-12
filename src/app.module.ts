dbb8dd5264111ccfe7ee15ad1a1e557c97b5f3f2
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-12 09:19:41 -0500

    feat: generate the modules 'tasks' and 'users'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, UsersModule, TasksModule],
   controllers: [],
   providers: [],
 })
