a08d9777af02e8a010195c6a7d3ab287f091ad50
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-11 20:25:56 -0500

    feat: add new properties for production and others environments for the file 'registers.env.ts'

--- src/common/modules/config/envs/registers.env.ts
+++ src/common/modules/config/envs/registers.env.ts
@@ -3,8 +3,24 @@ import { ENV } from './variables.env';
 
 export default registerAs('registers', () => {
   if (ENV === 'production') {
-    return {};
+    return {
+      db: {
+        postgres: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    };
   }
 
-  return {};
+  return {
+    db: {
+      postgres: {
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        name: process.env.POSTGRES_DB,
+      },
+    },
+  };
 });
