0d23e655f43cdb5692d5495609ca7ad67e298af2
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-12 13:52:50 -0500

    feat: set configuration for endpoint 'getAllUsers'

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,16 @@
+import { User } from '@entities/users/user.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
+
+  async findAll() {
+    const usersList = await this.userRepo.find();
+    return usersList;
+  }
+}
