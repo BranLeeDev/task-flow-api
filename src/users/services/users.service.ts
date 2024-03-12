7d2156f0ea9d6cd705fdeb523fa193a65acc3718
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-12 14:14:01 -0500

    feat: add new method in UsersService called 'findUserById'

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,5 +1,5 @@
 import { User } from '@entities/users/user.entity';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '../dtos/users.dto';
@@ -13,6 +13,13 @@ export class UsersService {
   async findAll() {
     const usersList = await this.userRepo.find();
     return usersList;
+  }
+
+  async findUserById(userId: number) {
+    const user = await this.userRepo.findOneBy({ id: userId });
+    if (!user)
+      throw new NotFoundException(`Not found the user with id #${userId}`);
+    return user;
   }
 
   async create(createUserDto: CreateUserDto) {
