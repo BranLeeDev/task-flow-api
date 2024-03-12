29f6521e493c21b8f714f8f67fbc7bd321fcb376
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-12 10:21:56 -0500

    feat: configure ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { PORT } from '@env/variables.env';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,6 +14,15 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.useLogger(app.get(Logger));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(PORT);
 }
 bootstrap();
