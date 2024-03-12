362388d5ac240ba4a6a548be416cbd0401628cb5
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-12 07:31:01 -0500

    feat: add 'base' entity

---
+++ src/common/modules/database/entities/shared/base.entity.ts
@@ -0,0 +1,24 @@
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Base {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+    name: 'updated_at',
+  })
+  updatedAt: Date;
+}
