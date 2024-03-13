a59f41ca6ddfa062d9d00cbe4e37609a5fd6d396
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-12 19:27:39 -0500

    feat: hash a user password

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FindManyOptions, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { User } from '@entities/users/user.entity';
 import { CreateUserDto, FilterUserDto, UpdateUserDto } from '../dtos/users.dto';
 
@@ -35,6 +36,8 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto) {
     const newUser = this.userRepo.create(createUserDto);
+    const hashPassword = await bcrypt.hash(newUser.password, 10);
+    newUser.password = hashPassword;
     const createdUser = await this.userRepo.save(newUser);
     return createdUser;
   }
