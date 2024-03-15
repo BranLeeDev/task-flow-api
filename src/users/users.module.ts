2a888bf2521ad7bc042f70a8da2d581963abc8a0
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-15 14:47:38 -0500

    feat: generate 'TeamsService' and 'TeamsController' using Nest CLI

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@entities/users/user.entity';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
+import { TeamsService } from './services/teams.service';
+import { TeamsController } from './controllers/teams.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
-  controllers: [UsersController],
+  providers: [UsersService, TeamsService],
+  controllers: [UsersController, TeamsController],
   exports: [UsersService],
 })
 export class UsersModule {}
