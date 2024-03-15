9b157cb23c95d73fab1c152e90ed03eb29279398
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-15 14:55:41 -0500

    feat: sort imports and add 'Team' entity to 'TypeOrmModule' module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,11 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@entities/users/user.entity';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
+
+// Teams
+import { Team } from '@entities/index';
 import { TeamsService } from './services/teams.service';
 import { TeamsController } from './controllers/teams.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Team])],
   providers: [UsersService, TeamsService],
   controllers: [UsersController, TeamsController],
   exports: [UsersService],
