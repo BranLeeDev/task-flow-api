1a8e9131fe260a5800daab3212be27bc506cb08b
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-15 08:14:56 -0500

    feat: export entities through 'index.ts' file

---
+++ src/common/modules/database/entities/index.ts
@@ -0,0 +1,4 @@
+export { Project } from './tasks/project.entity';
+export { Task } from './tasks/task.entity';
+export { Team } from './users/team.entity';
+export { User } from './users/user.entity';
