f7678c5249f5b7ab6178af4f3f458fa785188660
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-19 14:41:07 -0500

    feat: generate 'auth' module using Nest CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CommonModule, UsersModule, TasksModule],
+  imports: [CommonModule, UsersModule, TasksModule, AuthModule],
   controllers: [],
   providers: [],
 })
