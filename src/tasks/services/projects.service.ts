065db10fe578bc8df9a1a4bc7ff659a8e9541b27
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-19 13:15:00 -0500

    fix(projects.service): Rename 'projectsRepo' to 'projectRepo'

--- src/tasks/services/projects.service.ts
+++ src/tasks/services/projects.service.ts
@@ -13,28 +13,28 @@ export class ProjectsService {
 
   constructor(
     @InjectRepository(Project)
-    private readonly projectsRepo: Repository<Project>,
+    private readonly projectRepo: Repository<Project>,
     private readonly usersService: UsersService,
     private readonly teamsService: TeamsService,
   ) {}
 
   async findAll() {
     this.logger.log('Fetching all projects');
-    const projectsList = await this.projectsRepo.find();
+    const projectsList = await this.projectRepo.find();
     this.logger.log('All projects fetched successfully');
     return projectsList;
   }
 
   async create(createProjectDto: CreateProjectDto) {
     this.logger.log('Creating project');
-    const newProject = this.projectsRepo.create(createProjectDto);
+    const newProject = this.projectRepo.create(createProjectDto);
     newProject.manager = await this.usersService.findUserById(
       createProjectDto.managerId,
     );
     newProject.team = await this.teamsService.findTeamById(
       createProjectDto.teamId,
     );
-    const createdProject = await this.projectsRepo.save(newProject);
+    const createdProject = await this.projectRepo.save(newProject);
     this.logger.log('Project created successfully with ID');
     return createdProject;
   }
