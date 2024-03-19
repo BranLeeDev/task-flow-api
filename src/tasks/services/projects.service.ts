73233249c3a06b3f852f7d1c5a0fb0187387689e
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-19 10:08:35 -0500

    feat(projects.service): add logs with logger from '@nestjs/common'

--- src/tasks/services/projects.service.ts
+++ src/tasks/services/projects.service.ts
@@ -1,17 +1,22 @@
 import { Project } from '@entities/index';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProjectsService {
+  private readonly logger = new Logger(ProjectsService.name);
+
   constructor(
     @InjectRepository(Project)
     private readonly projectsRepo: Repository<Project>,
   ) {}
 
   async findAll() {
+    this.logger.log('Fetching all projects');
     const projectsList = await this.projectsRepo.find();
+    this.logger.log('All projects fetched successfully');
     return projectsList;
   }
 }
