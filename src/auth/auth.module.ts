050f102bfeef1d4a6490c0efbf9afa146bbee506
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-20 16:02:54 -0500

    feat(auth.module): import 'JwtModule' to imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { LoginService } from './services/login.service';
 import { AuthController } from './controllers/auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule, JwtModule],
   providers: [LoginService, LocalStrategy],
   controllers: [AuthController],
 })
