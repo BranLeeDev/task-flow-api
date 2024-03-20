427b4fd1e3d9a5960b37fa97a51ffee9cb476373
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-20 10:44:10 -0500

    feat(joi.lib): Add support for new environment variables
    This commit adds support for configuring JWT secrets and expiration times:
    - JWT_ACCESS_TOKEN_SECRET
    - JWT_ACCESS_TOKEN_EXPIRATION_TIME
    - JWT_REFRESH_TOKEN_SECRET
    - JWT_REFRESH_TOKEN_EXPIRATION_TIME

--- src/common/modules/config/libs/joi.lib.ts
+++ src/common/modules/config/libs/joi.lib.ts
@@ -52,4 +52,8 @@ export const joiConfigSchema = Joi.object({
     is: DEV,
     then: PORT,
   }),
+  JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
+  JWT_ACCESS_TOKEN_EXPIRATION_TIME: Joi.string().required(),
+  JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
+  JWT_REFRESH_TOKEN_EXPIRATION_TIME: Joi.string().required(),
 });
