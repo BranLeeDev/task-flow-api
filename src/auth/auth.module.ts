066e5847df0929af4610bcc29bf03ce2dd0534d4
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-19 19:12:33 -0500

    feat(auth.module): import 'UsersModule'

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './services/login.service';
 import { AuthController } from './controllers/auth.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   providers: [LoginService],
   controllers: [AuthController],
 })
