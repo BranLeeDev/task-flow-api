f4d5370041cd27a81fd7860a0dfbc4cbfd1f8ed9
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-19 19:04:03 -0500

    feat: generate 'auth.controller' using Nest CLI

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './services/login.service';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   providers: [LoginService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
