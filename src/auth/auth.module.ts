ccc30bf58da7358e8a67c4c4b17fffcd70e16241
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-20 08:18:24 -0500

    feat(auth.module): import 'LocalStrategy' in 'providers'

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { PassportModule } from '@nestjs/passport';
 import { LoginService } from './services/login.service';
 import { AuthController } from './controllers/auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [UsersModule, PassportModule],
-  providers: [LoginService],
+  providers: [LoginService, LocalStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
