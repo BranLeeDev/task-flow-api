5c650372d000d58a41b3ff001160dc1c035b6852
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-20 07:44:30 -0500

    feat(login.service): add 'validateUser' with all configuration

--- src/auth/services/login.service.ts
+++ src/auth/services/login.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/services/users.service';
+import { SignInDto } from '../dtos/login.dto';
 
 @Injectable()
-export class LoginService {}
+export class LoginService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async validateUser(signInDto: SignInDto) {
+    const { email, password } = signInDto;
+    const user = await this.usersService.findUserByEmail(email);
+    if (!user) return null;
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return null;
+    return user;
+  }
+}
