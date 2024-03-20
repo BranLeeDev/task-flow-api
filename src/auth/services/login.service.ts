beb3cd1311904618a2f25bb494d881ce81a0bc5d
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-20 12:27:10 -0500

    feat(login.service): add 'getCookieWithJwtAccessToken' and 'getCookieWithJwtRefreshToken' methods with all configuration

--- src/auth/services/login.service.ts
+++ src/auth/services/login.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/users/services/users.service';
 import { SignInDto } from '../dtos/login.dto';
+import registersEnv from '@env/registers.env';
+import { ConfigType } from '@nestjs/config';
+import { PayloadToken } from '../models/token.model';
+import { User } from '@entities/index';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class LoginService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+    @Inject(registersEnv.KEY)
+    private readonly configService: ConfigType<typeof registersEnv>,
+  ) {}
 
   async validateUser(signInDto: SignInDto) {
     const { email, password } = signInDto;
@@ -14,5 +24,32 @@ export class LoginService {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return null;
     return user;
+  }
+
+  getCookieWithJwtAccessToken(user: User) {
+    const { accessTokenSecret, accessTokenExpirationTime } =
+      this.configService.jwt;
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    const token = this.jwtService.sign(payload, {
+      secret: accessTokenSecret,
+      expiresIn: `${accessTokenExpirationTime}h`,
+    });
+    const cookie = `Authentication=${token}; HttpOnly; Path=/; Max-Age=${accessTokenExpirationTime}`;
+    return cookie;
+  }
+
+  getCookieWithJwtRefreshToken(user: User) {
+    const { refreshTokenSecret, refreshTokenExpirationTime } =
+      this.configService.jwt;
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    const token = this.jwtService.sign(payload, {
+      secret: refreshTokenSecret,
+      expiresIn: `${refreshTokenExpirationTime}h`,
+    });
+    const cookie = `Refresh=${token}; HttpOnly; Path=/; Max-Age=${refreshTokenExpirationTime}`;
+    return {
+      cookie,
+      token,
+    };
   }
 }
