b0a89f925d17001a633b03175e3d37c9fb1268de
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-20 11:10:57 -0500

    feat(migrations): generate 'add-current-hashed-refresh-token-column' migration

---
+++ src/common/modules/database/migrations/1710950977511-add-current-hashed-refresh-token-column.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddCurrentHashedRefreshTokenColumn1710950977511
+  implements MigrationInterface
+{
+  name = 'AddCurrentHashedRefreshTokenColumn1710950977511';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "current_hashed_refresh_token" character varying`,
+    );
+    await queryRunner.query(
+      `ALTER TYPE "public"."users_role_enum" RENAME TO "users_role_enum_old"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."users_role_enum" AS ENUM('administrator', 'project-manager', 'team-member', 'leader')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" TYPE "public"."users_role_enum" USING "role"::"text"::"public"."users_role_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'team-member'`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."users_role_enum_old"`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."users_role_enum_old" AS ENUM('administrator', 'project-manager', 'team-member')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" TYPE "public"."users_role_enum_old" USING "role"::"text"::"public"."users_role_enum_old"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'team-member'`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
+    await queryRunner.query(
+      `ALTER TYPE "public"."users_role_enum_old" RENAME TO "users_role_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" DROP COLUMN "current_hashed_refresh_token"`,
+    );
+  }
+}
