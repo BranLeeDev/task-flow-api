4d1565edac723d692947bd8121c5f898ba033a50
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-20 08:22:16 -0500

    feat(auth.controller): add 'signIn' method with initial configuration for login

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { SignInDto } from '../dtos/login.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { LoginService } from '../services/login.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly loginsService: LoginService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async signIn(@Body() signInDto: SignInDto) {
+    const res = await this.loginsService.validateUser(signInDto);
+    return res;
+  }
+}
