0dd8043a36916be80e085c3e481ac5ee6847af77
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-20 16:13:44 -0500

    feat(auth.controller): Implement refresh token functionality in 'login' endpoint

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,16 +1,36 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { SignInDto } from '../dtos/login.dto';
+import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { FastifyReply } from 'fastify';
+import { SignInDto } from '../dtos/login.dto';
 import { LoginService } from '../services/login.service';
+import { UsersService } from 'src/users/services/users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly loginsService: LoginService) {}
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  async signIn(@Body() signInDto: SignInDto) {
-    const res = await this.loginsService.validateUser(signInDto);
-    return res;
+  async signIn(
+    @Body() signInDto: SignInDto,
+    @Res({ passthrough: true }) res: FastifyReply,
+  ) {
+    const user = await this.usersService.findUserByEmail(signInDto.email);
+    const accessTokenCookie =
+      this.loginService.getCookieWithJwtAccessToken(user);
+    const { cookie: refreshTokenCookie, token: refreshToken } =
+      this.loginService.getCookieWithJwtRefreshToken(user);
+    await this.usersService.setCurrentRefreshToken(refreshToken, user.id);
+    res.header('set-cookie', [accessTokenCookie, refreshTokenCookie]);
+    return {
+      user,
+      cookies: {
+        accessTokenCookie,
+        refreshTokenCookie,
+      },
+    };
   }
 }
