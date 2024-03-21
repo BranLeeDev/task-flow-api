4711e27e7eb6e8f7b7966e66d4cf9d3608fe4303
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-21 13:43:47 -0500

    feat(logout.service): add 'getCookiesForLogout' method with all configuration

--- src/auth/services/logout.service.ts
+++ src/auth/services/logout.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import registersEnv from '@env/registers.env';
 
 @Injectable()
-export class LogoutService {}
+export class LogoutService {
+  constructor(
+    @Inject(registersEnv.KEY)
+    private readonly config: ConfigType<typeof registersEnv>,
+  ) {}
+
+  getCookiesForLogout(): string[] {
+    const { isProd } = this.config;
+    const secure = isProd ? 'Secure' : '';
+
+    return [
+      `Authentication=; HttpOnly; Path=/; Max-Age=0; ${secure}`,
+      `Refresh=; HttpOnly; Path=/; Max-Age=0; ${secure}`,
+    ];
+  }
+}
