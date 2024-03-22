d84937b3d7400fe17de9bf95c6b6e8b609df88be
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-22 07:43:49 -0500

    feat: import JwtStrategy and configure 'JwtAuthGuard' in 'providers'

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,12 +7,20 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtRefreshTokenStrategy } from './strategies/jwt-refresh-token.strategy';
 import { LogoutService } from './services/logout.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Module({
   imports: [UsersModule, PassportModule, JwtModule],
   providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
     LoginService,
     LocalStrategy,
+    JwtStrategy,
     JwtRefreshTokenStrategy,
     LogoutService,
   ],
