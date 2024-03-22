af02812035a9389af7e59b62c5d9d6b7d2383316
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-22 11:51:13 -0500

    feat: generate 'emails' module using Nest CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { EmailsModule } from './emails/emails.module';
 
 @Module({
-  imports: [CommonModule, UsersModule, TasksModule, AuthModule],
+  imports: [CommonModule, UsersModule, TasksModule, AuthModule, EmailsModule],
   controllers: [],
   providers: [],
 })
