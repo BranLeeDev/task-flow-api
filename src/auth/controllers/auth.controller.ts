3a32b6750b833b3be2a7139de6ce5f8af329c385
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-22 07:45:13 -0500

    feat(auth.controller): configure 'log-out' endpoint

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -4,14 +4,18 @@ import { FastifyReply } from 'fastify';
 import { LoginService } from '../services/login.service';
 import { UsersService } from 'src/users/services/users.service';
 import { FastifyRequest } from '../models/request.model';
+import { LogoutService } from '../services/logout.service';
+import { Public } from '../decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly loginService: LoginService,
+    private readonly logoutService: LogoutService,
     private readonly usersService: UsersService,
   ) {}
 
+  @Public()
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async signIn(
@@ -45,5 +49,17 @@ export class AuthController {
     );
     res.header('set-cookie', accessTokenCookie);
     return req.user;
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('log-out')
+  async logOut(
+    @Req() req: FastifyRequest,
+    @Res({ passthrough: true }) res: FastifyReply,
+  ) {
+    const { user } = req;
+    await this.usersService.removeRefreshToken(user.id);
+    res.header('set-cookie', this.logoutService.getCookiesForLogout());
+    return { message: 'User logged out successfully' };
   }
 }
