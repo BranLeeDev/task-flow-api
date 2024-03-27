813896f23d883eaa1e436407d1e47730433ec7bf
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-27 10:59:05 -0500

    feat(users.service): add 'leader' relation to 'findOne' method

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -72,7 +72,7 @@ export class UsersService {
     this.logger.log(`Fetching user with ID ${userId} with its relations`);
     const user = await this.userRepo.findOne({
       where: { id: userId },
-      relations: ['tasks', 'teams'],
+      relations: ['tasks', 'teams', 'leader'],
     });
     if (!user) {
       this.logger.error(`Not found the user with id #${userId}`);
