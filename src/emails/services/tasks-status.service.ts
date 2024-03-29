715fd996ae344ec8189e8815aeacbf89a98b7bfd
Brandon <brandonaguerodeveloper@gmail.com>
2024-03-29 10:45:15 -0500

    feat(tasks-status.service): add 'sendTasksStatus' method with all configuration

--- src/emails/services/tasks-status.service.ts
+++ src/emails/services/tasks-status.service.ts
@@ -1,4 +1,37 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { TasksService } from 'src/tasks/services/tasks.service';
+import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
-export class TasksStatusService {}
+export class TasksStatusService {
+  constructor(
+    private readonly tasksService: TasksService,
+    private readonly usersService: UsersService,
+    private readonly mailerService: MailerService,
+  ) {}
+
+  @Cron(CronExpression.EVERY_5_SECONDS)
+  async sendTasksStatus() {
+    const usersList = await this.usersService.findAll();
+
+    for (const user of usersList) {
+      const pendingTasks = await this.tasksService.countPendingTasks(user.id);
+      const inProgressTask = await this.tasksService.countInProgressTasks(
+        user.id,
+      );
+
+      await this.mailerService.sendMail({
+        to: user.email,
+        subject: 'Status of your tasks',
+        template: './tasks-status',
+        context: {
+          name: user.email.split('@')[0],
+          pendingTasks,
+          inProgressTask,
+        },
+      });
+    }
+  }
+}
